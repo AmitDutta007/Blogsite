f8486ad9fa948e4525d658cf23bbd96f840b94fb
amitdutta <duttaamit356@gmail.com>
2024-03-09 22:44:37 +0530

    created header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
 
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components';
+// import { Header } from './components/Header/Header';
+// import { Footer } from './components/Footer/Footer';
+
+
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
   return (
-    <>
-      <h1>Appwrite</h1>
-    </>
+    !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+
+          </main>
+          <Footer />
+        </div>
+      </div>
+      ) : null
   )
 }
 
